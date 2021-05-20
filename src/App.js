deb49fc06c0951cd6f0da2590c16bc86aab11fa1
William Truscott <williamtruscott@Williams-MacBook-Pro.local>
2021-05-20 12:42:46 -0400

    Setting up basics in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Link, Switch } from "react-router-dom";
+import Display from "./Display";
+import Form from "./Form";
 
 function App() {
+
+const url = ""
+
+const [songs, setSongs] = React.useState([])
+
+const emptySong = {
+  title: "",
+  artist: "",
+  duration: ""
+}
+
+const [selectedSong, setSelectedSong] = React.useState(emptySong)
+
+const getSongs = () => {
+fetch(url + "/tunes/")
+.then((response) => response.json())
+.then((data) => {
+  setSongs(data)
+})
+}
+
+React.useEffect(() => {
+  getSongs()
+}, [])
+
+const handleCreate = (newSong) => {
+fetch(url + "/tunes/", {
+  method: "POST",
+  headers: {
+    "Content-Type":"application/json"
+  },
+  body: JSON.stringify(newSong)
+})
+.then(() => getSongs())
+}
+
+const deleteSong = (song) => {
+fetch(url + "/tunes/" + song._id, {
+  method: "delete"
+})
+.then(() => {
+  getSongs()
+})
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+     <Heder/>
+     <Form
+      label="create"
+      song={emptySong}
+      handleSubmit={handleCreate}
+      />
+    </main>
   );
 }
 
